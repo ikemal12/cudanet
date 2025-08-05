e30195b3e3c44e8ee50a2d7898fbed0bb90728f7
Ilter Kemal <iltar2005@gmail.com>
2025-08-05 21:40:53 +0100

    wrapper function

--- src/conv2d.cu
+++ src/conv2d.cu
@@ -22,3 +22,35 @@ __global__ void conv2d_kernel(const float* input, const float *kernel, float* ou
         output[y * outputWidth + x] = sum;
     }
 }
+
+
+void conv2d(const float* input, const float* kernel, float* output,
+            int inputWidth, int inputHeight, int kernelWidth, int kernelHeight) {
+    
+    int outputWidth = inputWidth - kernelWidth + 1;
+    int outputHeight = inputHeight - kernelHeight + 1;
+
+    float *d_input, *d_kernel, *d_output;
+
+    cudaMalloc(&d_input, sizeof(float) * inputWidth * inputHeight);
+    cudaMalloc(&d_kernel, sizeof(float) * kernelWidth * kernelHeight);
+    cudaMalloc(&d_output, sizeof(float) * outputWidth * outputHeight);
+
+    cudaMemcpy(d_input, input, sizeof(float) * inputWidth * inputHeight, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_kernel, kernel, sizeof(float) * kernelWidth * kernelHeight, cudaMemcpyHostToDevice);
+
+    dim3 blockSize(16, 16);
+    dim3 gridSize((outputWidth + blockSize.x - 1) / blockSize.x,
+                  (outputHeight + blockSize.y - 1) / blockSize.y);
+
+    conv2d_kernel<<<gridSize, blockSize>>>(d_input, d_kernel, d_output,
+                                           inputWidth, inputHeight,
+                                           kernelWidth, kernelHeight);
+
+    cudaDeviceSynchronize();
+    cudaMemcpy(output, d_output, sizeof(float) * outputWidth * outputHeight, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_input);
+    cudaFree(d_kernel);
+    cudaFree(d_output);
+}
